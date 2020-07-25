f84b41cf8d644695d556e01c3efc76afe11f92dd
Nwosu-Promise <promiseokechukwu79@gmail.com>
2020-07-25 10:51:41 +0100

    code clean up

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const {parse} =require('querystring')
 
 const server = http.createServer ((req, res)=>{
   const {query, pathname} = url.parse(req.url, true)
+
   if (pathname === '/' && req.method==="GET" ) {
     res.end(`Hello World, Welcome to WeJapa Internships`)
-    // console.log(req.body);
   }
+
   else  if (pathname === '/' && req.method ==="POST") {
     let body = '';
     req.on('data', chunk => {
@@ -17,14 +18,13 @@ const server = http.createServer ((req, res)=>{
         console.log(
             parse(body)
         );
-
-        res.end(`Helo ${body.split('=')[1]}, Welcome to WeJapa Internships `);
+        res.end(`Hello ${body.split('=')[1]}, Welcome to WeJapa Internships `);
     });
 
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
-      // 'my-own-header': 'hello-world',
+      'my-own-header': 'hello-world',
     });
     res.end('<h1>Page not found!</h1>');
   }
@@ -33,5 +33,4 @@ const server = http.createServer ((req, res)=>{
 
 server.listen(8000,'127.0.0.1', ()=>{
   console.log(`Listening to request on port 8000`);
-
 })
